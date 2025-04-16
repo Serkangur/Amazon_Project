792ebc0c4557ec749417bd6e9e3e2c2a0c42eb3a
serkan <serkangur002@gmail.com>
2025-04-16 09:48:51 +0300

    Amazon Project

--- src/main/java/constants/ConstantsProductPage.java
+++
@@ -1,8 +0,0 @@
-package constants;
-
-import org.openqa.selenium.By;
-
-public class ConstantsProductPage {
-    public static By addToCart=By.id("submit.add-to-cart");
-    public static By productTitle=By.cssSelector(".product-title-word-break");
-}
